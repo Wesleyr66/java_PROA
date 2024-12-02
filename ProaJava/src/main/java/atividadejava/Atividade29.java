a097b98e6dc6879650ca88497dad999569f2e902
wesley <wesley.rochao@hotmail.com>
2024-12-02 09:50:52 -0300

    Update Atividade29.java

--- ProaJava/src/main/java/atividadejava/Atividade29.java
+++ ProaJava/src/main/java/atividadejava/Atividade29.java
@@ -6,6 +6,22 @@ public class Atividade29 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        System.out.println("Qual o nome:");
+        String nome = scan.next();
+        System.out.println("Entre com a nota 1:");
+        float nota = scan.nextFloat();
+        System.out.println("Entre com a nota 2:");
+        float nota2 = scan.nextFloat();
+        System.out.println("Entre com a nota 3:");
+        float nota3 = scan.nextFloat();
+
+        nota = (nota+nota2+nota3)/3;
+
+        if (nota<7){
+            System.out.println(nome + "Reprovado");
+        }else {
+            System.out.println(nome + "Aprovado");
+        }
         
     }
 }
